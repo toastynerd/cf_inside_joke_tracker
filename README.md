92fa55d550c647de9b5b4c744059d1f2a0a37e71
tylermorgan86 <tylermorgan86@gmail.com>
2013-05-22 12:46:22 -0700

    initial Commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+Inside Joke Tracker for Books
+===============================
+A small app a day for code fellows that tracks
+the inside jokes that come up in books and 
+demonstrating how the has_many_through
+relationship works.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InsideJokeTracker::Application.config.secret_token = '472809660ea7f27cd552396826f1c0d33f5baf4f708a09c9d897cc34c12942f6419a41f5e61bffbf9c52e2c84987a24f247fd3f610a7772364d57373ee3f9be2'
